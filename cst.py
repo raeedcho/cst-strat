07778cbdd71bb29d4c667b5a3e831d7ede4e8c24
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2021-06-15 18:52:16 -0400

    feat(cst module): add unfinished cst module to dump code that should not be in notebook

--- cst.py
+++ cst.py
@@ -1,30 +1,32 @@
 import mat73
 import pandas as pd
+import pyaldata
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-def get_cst_dataframe(filename):
-    mat = mat73.loadmat(filename)
-    trial_data = mat.trial_data
-    trial_data = {
-            'monkey': trial_data['monkey'],
-            'date_time': trial_data['date_time'],
-            'task': trial_data['task'],
-            'trial_id': trial_data['trial_id'],
-            'result': trial_data['result'],
-            'bin_size': trial_data['bin_size'],
-            'lambda': trial_data['lambda'],
-            'ct_location': trial_data['ct_location'],
-            'ot_location': trial_data['ot_location'],
-            'idx_startTime': trial_data['idx_startTime'],
-            'idx_endTime': trial_data['idx_endTime'],
-            'idx_ctHoldTime': trial_data['idx_ctHoldTime'],
-            'idx_goCueTime': trial_data['idx_goCueTime'],
-            'idx_otHoldTime': trial_data['idx_otHoldTime'],
-            'idx_rewardTime': trial_data['idx_rewardTime'],
-            'idx_failTime': trial_data['idx_failTime'],
-            'cursor_pos': trial_data['cursor_pos'],
-            'hand_pos': trial_data['hand_pos'],
-            'M1_unit_guide': trial_data['M1_unit_guide'],
-            'M1_spikes': trial_data['M1_spikes'],
-            }
+# this is unfinished...
+def plot_sensorimotor(trial, ax=None, color_by_state=False):
+    if ax is None:
+        ax = plt.gca()
 
-    return pd.DataFrame.from_dict(trial_data)
+    plt.plot([-60,60],[60,-60],'--k')
+    plt.plot([0,0],[-60,60],'-k')
+    plt.plot([-60,60],[0,0],'-k')
+    if color_by_state:
+        plt.scatter(
+            trial['cursor_pos'][:,0],
+            trial['hand_pos'][:,0],
+            c=hmm_z,
+            cmap=cmap,
+            s=10,
+            norm=mpl.colors.Normalize(vmin=0,vmax=len(color_names)-1)
+            )
+    else:
+        plt.scatter(trial['cursor_pos'][:,0],trial['hand_pos'][:,0],c='k',s=10)
+        plt.xticks([])
+        plt.yticks([])
+        plt.xlim(-scale,scale)
+        plt.ylim(-scale,scale)
+        plt.xlabel('Cursor position')
+        plt.ylabel('Hand position')
+        sns.despine(left=True,bottom=True)
