e000340e08c94351f0b83e86af07f38b570ed736
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-01-04 18:07:43 -0500

    refactor: add cst as a module to pipenv using setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cst',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for running CST analyses',
+    author='Raeed Chowdhury',
+)
+
+
