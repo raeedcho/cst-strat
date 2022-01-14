9bdd54f3d73be2406e4e9331b86198ed74fa4729
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-01-14 18:03:34 -0500

    feat(tuning comparison): add script and code to compare neural tuning across tasks

---
+++ analysis-scripts/compare_task_neural_tuning.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+# This script runs a single neuron tuning comparison between center-out and CST
+
+import cst
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def main(args):
+    td = cst.load_clean_data(args.infile,args.verbose)
+    lr_selectivity_table = cst.single_neuron_analysis.get_task_neural_lr_selectivity(td,signal='M1_spikes')
+    
+    outfile_name = '{monkey}_{session_date}_lr_selectivity.feather'.format(
+        monkey=td['monkey'].values[0],
+        session_date=np.datetime_as_string(td['session_date'].values[0],'D').replace('-','')
+    )
+    lr_selectivity_table.to_feather(os.path.join(args.outdir,outfile_name))
+
+    sns.set_context('talk')
+    cst.plot_task_neural_lr_selectivity(lr_selectivity_table)
+    fig_outfile_name = '{monkey}_{session_date}_task_lr_selectivity.png'.format(
+        monkey=td['monkey'].values[0],
+        session_date=np.datetime_as_string(td['session_date'].values[0],'D').replace('-','')
+    )
+    plt.savefig(os.path.join(args.outdir,fig_outfile_name))
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Run single neuron analyses')
+    parser.add_argument('--infile',required=True,help='path to input file')
+    parser.add_argument('--outdir',required=True,help='path to output directory for results and plots')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    args = parser.parse_args()
+
+    main(args)
--- cst/single_neuron_analysis.py
+++ cst/single_neuron_analysis.py
@@ -99,3 +99,97 @@ def plot_task_epoch_neural_averages(avg_fr_table):
                 ax.set_ylim([0,max_fr+5])
 
     plt.suptitle('Task/Epoch average firing rate comparison')
+
+def get_task_neural_lr_selectivity(trial_data,signal=''):
+    '''
+    Takes trial data as input and returns a table of left/right selectivity per neuron,
+    separated by array, task (CO or CST). Left/right selectivity is the coefficient
+    relating the x-axis velocity to the neuron's firing rate (units: (spikes/sec)/(m/sec)).
+    Essentially, this is the slope of the Poisson regression of the neuron's firing rate
+    vs. x-axis velocity.
+
+    Arguments:
+        trial_data (DataFrame): PyalData formatted structure of neural/behavioral data
+        signal (str): name of signal corresponding to neural data
+
+    Returns:
+        DataFrame: table of average tuning curves per neuron, with columns:
+            ['monkey','session_date','task','array','chan_id','unit_id','lr_selectivity']
+    '''
+    array = util.get_array_from_signal(signal)
+    unit_guide = trial_data.loc[trial_data.index[0], array+'_unit_guide']
+    tasks = ['CO', 'CST']
+
+    lr_selectivity_table_list = []
+    for task in tasks:
+        temp_td = trial_data.loc[trial_data['task']==task,:].copy()
+        temp_td = pyaldata.restrict_to_interval(
+            temp_td,
+            start_point_name='idx_goCueTime',
+            end_point_name='idx_endTime',
+            rel_end=-1,
+            reset_index=False
+        )
+        temp_td = pyaldata.combine_time_bins(temp_td,int(0.05//temp_td['bin_size'].values[0]))
+
+        lr_selectivity_table_list.append(pd.DataFrame({
+            'monkey': temp_td['monkey'].values[0],
+            'session_date': temp_td['session_date'].values[0],
+            'task': task,
+            'array': array,
+            'chan_id': unit_guide[:,0],
+            'unit_id': unit_guide[:,1],
+            'lr_selectivity': get_lr_selectivity(temp_td,signal).squeeze()
+        }))
+
+    lr_selectivity_table = pd.concat(lr_selectivity_table_list)
+    lr_selectivity_table.reset_index(drop=True,inplace=True)
+
+    return lr_selectivity_table
+
+def get_lr_selectivity(trial_data,signal):
+    '''
+    Takes trial data as input and returns a numpy array of left/right selectivity
+    per neuron in trial_data[signal]. Left/right selectivity is the coefficient
+    of the Poisson regression relating the x-axis hand velocity to the neuron's firing rate
+    (units: (spikes/sec)/(m/sec)).
+
+    Arguments:
+        trial_data (DataFrame): PyalData formatted structure of neural/behavioral data
+        signal (str): name of signal to use for calculating selectivity
+
+    Returns:
+        numpy array: array of left/right selectivity per neuron in trial_data[signal]
+    '''
+    hand_vel = np.concatenate(trial_data['hand_vel'].values)[:,0][:,np.newaxis]
+    neural_data = np.concatenate(trial_data[signal].values)
+
+    lr_sensitivity = np.zeros(neural_data.shape[1])
+    for neuronnum in range(neural_data.shape[1]):
+        lr_sensitivity_model = sklearn.linear_model.PoissonRegressor() # TODO: make type of regression a parameter
+        lr_sensitivity_model.fit(hand_vel,neural_data[:,neuronnum])
+        lr_sensitivity[neuronnum] = lr_sensitivity_model.coef_[0]
+
+    return lr_sensitivity
+
+def plot_task_neural_lr_selectivity(lr_selectivity_table):
+    '''
+    Takes a table of left/right selectivity per neuron, separated by array, task (CO or CST)
+    Note: this function alters the current state of matplotlib by using the pyplot state machine
+
+    Arguments:
+        lr_selectivity_table (DataFrame): table of left/right selectivity per neuron, with columns:
+            ['monkey','session_date','task','array','chan_id','unit_id','lr_selectivity']
+
+    Returns:
+        None
+    '''
+    lr_selectivity_pivot = lr_selectivity_table.pivot(index=['chan_id','unit_id'],columns='task',values='lr_selectivity')
+    plt.scatter(lr_selectivity_pivot['CO'],lr_selectivity_pivot['CST'])
+    plt.plot([-0.02,0.02],[-0.02,0.02],'--k')
+    plt.plot([-0.02,0.02],[0,0],'-k')
+    plt.plot([0,0],[-0.02,0.02],'-k')
+    plt.xlabel('CO sensitivity')
+    plt.ylabel('CST sensitivity')
+    plt.suptitle('Task neural left/right selectivity (Poisson)')
+    sns.despine()
