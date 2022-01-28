92387ac0d0effbdc375c463455ec6e0fc1bf9d01
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-01-28 17:49:41 -0500

    feat(engagement): add a stub script to look for single neuron engagement

---
+++ scripts/single_neuron_engagement_analysis.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+# This script runs a single neuron engagement analysis, looking for
+# how average neural activity changes as lambda changes
+
+import cst
+import pyaldata
+
+def main(args):
+    td = cst.load_clean_data(args.infile,args.verbose)
+
+    td_cst = td.loc[td['task']=='CST',:].copy()
+    td_cst = pyaldata.restrict_to_interval(
+        td_cst,
+        start_point_name='idx_cstStartTime',
+        end_point_name='idx_cstEndTime',
+        reset_index=False
+    )
+    td_cst = pyaldata.combine_time_bins()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Run single neuron analyses')
+    parser.add_argument('--infile',required=True,help='path to input file')
+    parser.add_argument('--outdir',required=True,help='path to output directory for results and plots')
+    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    args = parser.parse_args()
+
+    main(args)
