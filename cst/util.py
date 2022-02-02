84b7b28ae9b21e0f018c0a59779bc8d055288920
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-02-02 18:21:09 -0500

    feat(cst/util): add function to split trials into epochs residing in different trial_data entries

--- cst/util.py
+++ cst/util.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pyaldata
+import pandas as pd
 
 def format_outfile_name(trial_data,postfix=''):
     '''
@@ -26,3 +28,29 @@ def get_array_from_signal(signal):
         return signal.replace('_rate','')
     else:
         raise ValueError('signal must end in "_spikes" or "_rate"')
+
+def split_trials_by_epoch(trial_data,epoch_dict,epoch_col_name='epoch'):
+    '''
+    Split trial_data by epochs
+    
+    Arguments:
+        trial_data (DataFrame): PyalData formatted structure of neural/behavioral data
+        epoch_dict (dict): dictionary of epoch functions to pass into
+            pyaldata.restrict_to_interval, with keys as epoch names
+        epoch_col_name (str): name of column to add to trial_data with epoch names
+        
+    Returns:
+        DataFrame: trial_data with entries for each specified epoch of each trial
+    '''
+    td_epoch_list = []
+    for epoch_name,epoch_fun in epoch_dict.items():
+        temp_td = pyaldata.restrict_to_interval(
+            trial_data,
+            epoch_fun=epoch_fun,
+        )
+        temp_td[epoch_col_name] = epoch_name
+        td_epoch_list.append(temp_td)
+    
+    td_epoch = pd.concat(td_epoch_list).reset_index(drop=True)
+
+    return td_epoch
