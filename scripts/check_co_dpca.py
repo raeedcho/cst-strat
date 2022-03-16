f2ea53c6cbf7b898314124e094a027cc2ac3ee27
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2022-03-16 12:26:35 -0400

    feat(dpca): refactor dpca script to generate figues automatically

--- scripts/check_co_dpca.py
+++ scripts/check_co_dpca.py
@@ -14,7 +14,6 @@ def main(args):
     td = src.load_clean_data(args.infile,args.verbose)
 
     params = yaml.safe_load(open("params.yaml"))['co_dpca']
-    sns.set_context('talk')
 
     td = preprocess_neural_activity(td,params)
 
@@ -22,13 +21,10 @@ def main(args):
     td_co = td.groupby(['task','result']).get_group(('CO','R'))
     epoch_fun = src.generate_realtime_epoch_fun(
         'idx_goCueTime',
-        rel_start_time=params['rel_start_time'],
-        rel_end_time=params['rel_end_time'],
+        rel_start_time=params['co_rel_start_time'],
+        rel_end_time=params['co_rel_end_time'],
     )
     td_co = pyaldata.restrict_to_interval(td_co,epoch_fun=epoch_fun)
-
-    # center signals for just CO data for dPCA
-    td_co = pyaldata.center_signal(td_co,'M1_state')
 
     dpca,latent_dict = fit_dpca(td_co)
 
@@ -37,10 +33,10 @@ def main(args):
 
     # get CST data
     td_cst = td.groupby(['task','result']).get_group(('CST','R'))
-        'idx_cstStartTime',
-        rel_start_time=0,
-        rel_end_time=5,
     cst_epoch_fun = src.generate_realtime_epoch_fun(
+        'idx_goCueTime',
+        rel_start_time=params['cst_rel_start_time'],
+        rel_end_time=params['cst_rel_end_time'],
     )
     td_cst = pyaldata.restrict_to_interval(td_cst,epoch_fun=cst_epoch_fun)
 
@@ -48,11 +44,17 @@ def main(args):
     td_cst = add_dpca_projections(td_cst,dpca)
 
     # temp plotting
+    sns.set_context('talk')
     fig_gen_dict = {
-        'mean_co_dpca':plot_dpca(td_co,latent_dict,params),
-        'co_dpca_trials': plot_dpca_projection(td_co,params),
-        'cst_dpca_trials': plot_dpca_projection(td_cst,params),
+        'mean_co_dpca':plot_dpca(td_co,latent_dict),
+        'co_dpca_trials': plot_dpca_projection(td_co),
+        'cst_dpca_trials': plot_dpca_projection(td_cst),
+        'co_dpca_cis_traces': plot_cis_traces(td_co),
+        'cst_dpca_cis_traces': plot_cis_traces(td_cst),
     }
+
+    if not os.path.isdir(args.outdir):
+        os.mkdir(args.outdir)
 
     for fig_postfix,fig in fig_gen_dict.items():
         fig_name = src.format_outfile_name(td,postfix=fig_postfix)
@@ -117,9 +119,8 @@ def add_dpca_projections(td,dpca):
 
     return td
 
-def plot_dpca(td,latent_dict,params):
-    # TODO: fix this to only rely on the trial data
-    timevec = np.arange(td['M1_state'].values[0].shape[0])*td['bin_size'].values[0]+params['rel_start_time']
+def plot_dpca(td,latent_dict):
+    timevec = np.arange(td['M1_state'].values[0].shape[0])*td['bin_size'].values[0]-td['idx_goCueTime'].values[0]
     fig,ax = plt.subplots(2,5,figsize=(15,7),sharex=True,sharey=True)
 
     for condnum,(key,val) in enumerate(latent_dict.items()):
@@ -137,11 +138,11 @@ def plot_dpca(td,latent_dict,params):
 
     return fig
 
-def plot_dpca_projection(td,params):
+def plot_dpca_projection(td):
     '''
     Plot out dPCA projections from td
     '''
-    timevec = np.arange(td['M1_state'].values[0].shape[0])*td['bin_size'].values[0]+params['rel_start_time']
+    timevec = np.arange(td['M1_state'].values[0].shape[0])*td['bin_size'].values[0]-td['idx_goCueTime'].values[0]
     fig,ax = plt.subplots(2,5,figsize=(15,7),sharex=True,sharey=True)
 
     # tgt_colors = {0: '#1f77b4', 90: '#ff7f0e', 180: '#2ca02c', -90: '#d62728'}
@@ -164,6 +165,33 @@ def plot_dpca_projection(td,params):
     ax[1,0].set_ylabel('Target-related (Hz)')
     for component in range(5):
         ax[0,component].set_title(f'Component #{component+1}')
+    sns.despine(fig=fig,trim=True)
+
+    return fig
+
+def plot_cis_traces(td):
+    '''
+    Plot out top two time-related components of dPCA for all trials
+    TODO: make this general by asking for a signal and plotting all trials and average
+    '''
+    fig,ax = plt.subplots(1,1,figsize=(5,5))
+    for _,trial in td.iterrows():
+        ax.plot(
+            trial['M1_dpca_time'][:,0],
+            trial['M1_dpca_time'][:,1],
+            color=[0.5,0.5,0.5,0.2],
+        )
+
+    avg_trace = np.mean(np.stack(td['M1_dpca_time'],axis=0),axis=0)
+    ax.plot(
+        avg_trace[:,0],
+        avg_trace[:,1],
+        color='k',
+        linewidth=2,
+    )
+
+    ax.set_xlabel('Time component #1 (Hz)')
+    ax.set_ylabel('Time component #2 (Hz)')
     sns.despine(fig=fig,trim=True)
 
     return fig
